b93c273b1e3f5ca7d9472c260184aa2c4d184bc3
Ameskour <ameskour97@gmail.com>
2026-09-02 22:29:13 +0200

    tst

---
+++ after_42/C_function_pointers/ft_foreach.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+void ft_foreach(int *tab, int length, void(*f)(int)){
+    int i;
+    i = 0;
+    while(i < length){
+        f(tab[i]);
+        i++;
+    }
+}
+void ft_putnbr(int n){
+    if(n < 0){
+        write(1, "-", 1);
+        n = -n;
+    }
+    if(n >= 10){
+        ft_putnbr(n / 10);
+    }
+    char c = n % 10 + '0';
+    write(1, &c, 1);
+}
+
+int main(void){
+    int tab[5] = {1, 2, 3, 4, 5}; 
+    ft_foreach(tab, 5, &ft_putnbr);
+    write(1, "\n", 1);
+    return 0;
+}
